c6d21401ee49ba8150351b0e9763fd417163e4fd
Wei Liu <leohoo@hotmail.com>
2022-09-13 17:25:10 +0900

    Update ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -20,6 +20,6 @@ brew install cocoapods
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.
 
-flutter build ios --build-name=$APPVER
+flutter build ios --build-name=$APPVER --no-codesign
 
 #exit 0
